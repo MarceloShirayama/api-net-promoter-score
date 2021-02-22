c09f557df38c3702b574216d6892a4ad617b4262
MarceloShirayama <caixadecorreiodomarcelo@gmail>
2021-02-22 15:52:16 -0300

    create route '/' GET and POST

--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,10 @@ const app = express();
 const PORT: number = 3333;
 const HOST: string = '0.0.0.0';
 
+// app.get('/users', (request, response) => response.send('Hello World - NLW04'));
+app.get('/', (request, response) => response.json({ message: 'Hello World - NLW04' }));
+
+app.post('/', (request, response) => response.json({ message: 'Os dados foram salvos com sucesso' }));
+
 // eslint-disable-next-line no-console
 app.listen(PORT, HOST, () => console.log('Server is running!'));
